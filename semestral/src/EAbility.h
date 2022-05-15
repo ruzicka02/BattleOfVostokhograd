80d7cd2f7ae048478c472bba202d9856486ae122
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2022-05-15 23:20:45 +0200

    Sem: New cards and special abilities
    Added new csv with various cards for the final game. New abilities were
    included in cards but not implemented yet - discard (Your opponent
    discards a card) and sacrifice (You can sacrifice a card from your
    discard pile).

--- semestral/src/EAbility.h
+++ semestral/src/EAbility.h
@@ -14,10 +14,16 @@ enum EAbility {
 	steal,
 
 	/// Player can draw one extra card from his drawing deck
-	draw
+	draw,
+
+	/// Opponent player discards one card from hand in his next turn
+	discard,
+
+	/// Player can sacrifice (=destroy) card in his own discard pile
+	sacrifice
 };
 
-inline std::string ability_str(EAbility val) {
+inline std::string ability_to_print_str(EAbility val) {
 	switch (val) {
 		case null:
 			return "No ability";
@@ -25,9 +31,43 @@ inline std::string ability_str(EAbility val) {
 			return "Steal a card";
 		case draw:
 			return "Draw a card";
+		case discard:
+			return "Opponent discards a card";
+		case sacrifice:
+			return "Sacrifice a card";
 		default:
 			return "Unexpected value";
 	}
+}
+
+inline std::string ability_to_str(EAbility val) {
+	switch (val) {
+		case steal:
+			return "steal";
+		case draw:
+			return "draw";
+		case discard:
+			return "discard";
+		case sacrifice:
+			return "sacrifice";
+		default:
+			return "null";
+	}
+}
+
+inline EAbility str_to_ability(const std::string& val) {
+	EAbility special = null;
+
+	if ( val == "steal" )
+		special = steal;
+	else if ( val == "draw" )
+		special = draw;
+	else if ( val == "discard" )
+		special = discard;
+	else if ( val == "sacrifice" )
+		special = sacrifice;
+
+	return special;
 }
 
 #endif //SEMESTRAL_EABILITY_H
